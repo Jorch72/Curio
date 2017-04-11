8e17963b67ec8daea3cd9acceda42539f5c86c38
Tyler Hancock <darklime@live.ca>
2017-04-11 15:23:29 -0600

    Added a composite Curio implementation.

---
+++ src/main/java/net/darkhax/curio/api/curio/CurioComposite.java
@@ -0,0 +1,87 @@
+package net.darkhax.curio.api.curio;
+
+import javax.annotation.Nonnull;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+
+/**
+ * This implementation of ICurio is used as a base for curio which contains other curio. This
+ * implementation delegates the majority of calls to the contained curio.
+ */
+public abstract class CurioComposite implements ICurio {
+
+    /**
+     * A non null list of held curio. Used for
+     * {@link #getContainedCurio(ItemStack, EntityLivingBase)}.
+     */
+    private final NonNullList<ICurio> heldCurio = NonNullList.create();
+
+    /**
+     * Adds a curio to the composite.
+     *
+     * @param curio The curio to add.
+     */
+    public void addCurio (@Nonnull ICurio curio) {
+
+        this.heldCurio.add(curio);
+    }
+
+    /**
+     * Removes a curio from the composite.
+     *
+     * @param curio The curio to remove.
+     */
+    public void removeCurio (@Nonnull ICurio curio) {
+
+        this.heldCurio.remove(curio);
+    }
+
+    @Override
+    public boolean canEquip (@Nonnull ItemStack stack, @Nonnull EntityLivingBase wearer) {
+
+        for (final ICurio curio : this.heldCurio)
+            if (!curio.canEquip(stack, wearer))
+                return false;
+
+        return true;
+    }
+
+    @Override
+    public boolean canRemove (@Nonnull ItemStack stack, @Nonnull EntityLivingBase wearer) {
+
+        for (final ICurio curio : this.heldCurio)
+            if (!curio.canRemove(stack, wearer))
+                return false;
+
+        return true;
+    }
+
+    @Override
+    public void onEquipped (@Nonnull ItemStack stack, @Nonnull EntityLivingBase wearer) {
+
+        for (final ICurio curio : this.heldCurio)
+            curio.onEquipped(stack, wearer);
+    }
+
+    @Override
+    public void onRemoved (@Nonnull ItemStack stack, @Nonnull EntityLivingBase wearer) {
+
+        for (final ICurio curio : this.heldCurio)
+            curio.onRemoved(stack, wearer);
+    }
+
+    @Override
+    public void onUpdate (@Nonnull ItemStack stack, @Nonnull EntityLivingBase weaer) {
+
+        for (final ICurio curio : this.heldCurio)
+            curio.onUpdate(stack, weaer);
+    }
+
+    @Override
+    public NonNullList<ICurio> getContainedCurio (@Nonnull ItemStack stack, @Nonnull EntityLivingBase weaer) {
+
+        return this.heldCurio;
+    }
+}
