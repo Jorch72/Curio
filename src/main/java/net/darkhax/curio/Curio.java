14ff92b6a91cbf9439370a6c1cbf8f50d7d77f50
Tyler Hancock <darklime@live.ca>
2017-04-11 19:16:24 -0600

    Capability is now registered.

--- src/main/java/net/darkhax/curio/Curio.java
+++ src/main/java/net/darkhax/curio/Curio.java
@@ -1,6 +1,9 @@
 package net.darkhax.curio;
 
+import net.darkhax.curio.api.capabilities.CurioCapabilities;
+import net.darkhax.curio.api.curio.ICurio;
 import net.darkhax.curio.lib.Constants;
+import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -14,5 +17,6 @@ public class Curio {
     @EventHandler
     public void preInit (FMLPreInitializationEvent event) {
 
+        CapabilityManager.INSTANCE.register(ICurio.class, new CurioCapabilities.StorageCurio(), CurioCapabilities.CurioDefault.class);
     }
 }
