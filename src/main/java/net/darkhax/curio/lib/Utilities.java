36db8e1f3d7ad6e1adc538e689d353ac7bb28e9e
Tyler Hancock <darklime@live.ca>
2017-04-15 22:15:06 -0600

    Added a few basic utiltiies to help clean stuff up.

---
+++ src/main/java/net/darkhax/curio/lib/Utilities.java
@@ -0,0 +1,24 @@
+package net.darkhax.curio.lib;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+
+import net.darkhax.curio.Curio;
+import net.darkhax.curio.api.curio.ICurio;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+
+public class Utilities {
+
+    public static boolean isCurio (@Nonnull ItemStack stack) {
+
+        return !stack.isEmpty() && stack.hasCapability(Curio.CAPABILITY_CURIO, null);
+    }
+
+    public static List<ICurio> getCurioForStack (@Nonnull ItemStack stack, @Nonnull EntityLivingBase wearer) {
+
+        return isCurio(stack) ? stack.getCapability(Curio.CAPABILITY_CURIO, null).getContainedCurio(stack, wearer) : new ArrayList<>();
+    }
+}
