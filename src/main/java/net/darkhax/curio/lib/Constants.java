890442b862ddfbac62f6da40e65990c9c0cb8ed5
Tyler Hancock <darklime@live.ca>
2017-04-22 19:45:54 -0600

    Removed unused reference fields.

--- src/main/java/net/darkhax/curio/lib/Constants.java
+++ src/main/java/net/darkhax/curio/lib/Constants.java
@@ -1,7 +1,5 @@
 package net.darkhax.curio.lib;
 
-import java.util.Random;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,12 +10,6 @@ public class Constants {
     public static final String MOD_NAME = "Curio";
 
     public static final String VERSION_NUMBER = "1.0.0.0";
-
-    public static final String CLIENT_PROXY_CLASS = "net.darkhax.darkutils.client.ProxyClient";
-
-    public static final String SERVER_PROXY_CLASS = "net.darkhax.darkutils.common.ProxyCommon";
-
-    public static final Random RANDOM = new Random();
 
     public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 }
